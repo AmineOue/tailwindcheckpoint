4300f09cf5b94bcc77749b3c04a1c45c88a2db21
AmineOue <158207206+AmineOue@users.noreply.github.com>
2024-02-15 10:13:50 +0100

    Add files via upload

---
+++ checkpointtailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'back':"url('/backkk.jpg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
